dfc4e38c843ce38c553c93ed41f12d41c466fe17
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2025-05-20 12:38:34 +0200

    Windows features tested

--- src/engine/terminal-utils.cpp
+++ src/engine/terminal-utils.cpp
@@ -8,12 +8,18 @@
 // Implementazione
 int obtainTerminalSizes(int* widthDest, int* heightDest) {
 #ifdef _WIN32
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
-        cols = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-        rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    // Ottieni le informazioni dal servizio di Windows
+    CONSOLE_SCREEN_BUFFER_INFO consoleScreenBufferInfo;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &consoleScreenBufferInfo)) {
+        *widthDest = consoleScreenBufferInfo.srWindow.Right - consoleScreenBufferInfo.srWindow.Left + 1;
+        *heightDest = consoleScreenBufferInfo.srWindow.Bottom - consoleScreenBufferInfo.srWindow.Top + 1;
+        return 1; // Tutto OK
     }
+
+    // Segnala l'errore
+    return 0;
 #else
+    // Informazioni sistemi UNIX
     winsize winSize{};
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &winSize) == 0) {
         *widthDest = winSize.ws_row;
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 // C++ Libs
 #include <iostream>
+#include <unistd.h>
 
 // Libs
 #include "engine/terminal-utils.h"
@@ -15,5 +16,13 @@
 using namespace std;
 
 int main() {
+    while (true) {
+        int righe, colonne;
+        obtainTerminalSizes(&righe, &colonne);
+        cout << righe << endl;
+        cout << colonne << endl;
+        cout << endl;
+        usleep(1000000);
+    }
     return 0;
 }
